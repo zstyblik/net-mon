60b49066b156e64cc5c8e5000990196076e9d0b9
Zdenek Styblik <stybla@turnovfree.net>
2011-06-18 20:57:35 +0200

    MOD code formatting Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Signed-off-by: Zdeněk Styblík <stybla@turnovfree.net>

--- scripts/net-mon-nodemod.pl
+++ scripts/net-mon-nodemod.pl
@@ -6,64 +6,68 @@
 #
 # 2009/Sep/25 @ Zdenek Styblik
 #
-use Switch;
-
 my $dn = 'dc=turnovfree,dc=net';
-my $dnPeople = 'ou=people'.$dn;
+my $dnPeople = sprintf("ou=people,%s", $dn);
 
-sub addNode {
-## CN
+sub addNode 
+{
+	## CN
 	my $cn = undef;
-	while ($cn !~ /^[A-Za-z0-9].+$/) {
+	while ($cn !~ /^[A-Za-z0-9].+$/) 
+	{
 		print "Enter node name [foo]: ";
 		$cn = <STDIN>;
 		chomp($cn);
 		print "\n";
-	}
-## LOCATION
+	} # while $cn
+	## LOCATION
 	print "Enter location of node [moon]: ";
 	my $l = <STDIN>;
 	chomp($l);
 	print "\n";
-## PORT
+	## PORT
 	my $port = undef;
-	while ($port !~ /^[0-9].+$/) {
+	while ($port !~ /^[0-9].+$/) 
+	{
 		print "Port on node to poke at [80]: ";
 		$port = <STDIN>;
 		chomp($port);
 		print "\n";
-	}
-## IP
+	} # while $port
+	## IP
 	print "IP address of node [192.168.1.1]: ";
 	my $ip = <STDIN>;
 	chomp($ip);
 	print "\n";
-## PROTOCOL
+	## PROTOCOL
 	my $proto = undef;
 	my $go = 0;
-	while ($go == 0) {
+	while ($go == 0) 
+	{
 		print "Protocol to use [icmp/udp/tcp]: ";
 		$proto = <STDIN>;
 		chomp($proto);
 		print "\n";
-		switch ($proto) {
+		switch ($proto) 
+		{
 			case 'icmp'	{ $go = 1 }
 			case 'udp'	{ $go = 1 }
 			case 'tcp'	{ $go = 1 }
 			else				{ $go = 0 }
 		}
-	}
-## MANAGER
+	} # while $go
+	## MANAGER
 	my $managerUid = undef;
-	while ($managerUid !~ /^[A-Za-z0-9]{1,}+$/) {
+	while ($managerUid !~ /^[A-Za-z0-9]{1,}+$/) 
+	{
 		print "Manager of device\n";
 		print "0 - disable\n";
 		print "Manager UID [admin]: ";
 		$managerUid = <STDIN>;
 		chomp($managerUid);
 		print "\n";
-	}
-## WRITE FILE
+	} # while $managerUid
+	## WRITE FILE
 	my $file = '>'.$cn.'.add.ldif';
 	open(FILE, $file);
 	print FILE "# cn=$cn,ou=net-mon,$dn\n";
@@ -76,24 +80,27 @@ sub addNode {
 	print FILE "ipHostNumber: $ip\n";
 	print FILE "cn: $cn\n";
 	print FILE "ipServiceProtocol: $proto\n";
-	if ($managerUid !~ /^[0]{1}$/) {
+	if ($managerUid !~ /^[0]{1}$/) 
+	{
 		print FILE "manager: uid=$managerUid,$dnPeople\n";
-	}
+	} # if $managerUid
 	close FILE;
 	return 0;
-}
+} # sub addNode
 
-sub help {
+sub help 
+{
 	print "Net-mon Add node LDIF generator\n";
 	print "Help:\n";
 	print "	-a	add node\n";
 	print "	-h	print this help\n";
 	print "\n";
 	return 0;
-}
+} # sub help
 
 ## MAIN
-switch ($ARGV[0]) {
+switch ($ARGV[0]) 
+{
 	case '-a'	{ addNode }
 	case '-h'	{ help }
 	else			{ help }
