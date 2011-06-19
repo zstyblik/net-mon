a02257218556df4c86e2752eb05d3b62c9984549
Zdenek Styblik <stybla@turnovfree.net>
2011-06-19 06:41:23 +0200

    MOD code formatting; print -> printf etc. Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Signed-off-by: Zdeněk Styblík <stybla@turnovfree.net>

--- NetMon.pm
+++ NetMon.pm
@@ -1,9 +1,13 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 package NetMon;
 use DBI;
 use HTML::Template;
 
-sub start {
+use warnings;
+use strict;
+
+sub start 
+{
 	# DBI params;
 	my $dbiDSN = 'dbi:PgPP:dbname=net-mon;host=10.117.0.1;port=5432';
 	my $dbiUser = '';
@@ -12,46 +16,50 @@ sub start {
 	my $tmplPath = "./net-mon/";
 
 	my $dbh = DBI->connect($dbiDSN, $dbiUser, $dbiPswd);
-	if (!$dbh) {
+	if (!$dbh) 
+	{
 			my $msg = "Unable to connect do DB";
 			return $msg;
-	};
+	} # if !$dbh
 
 	my $sqlSMaxTime = "SELECT MAX(log_time) AS max_log_time \
 	FROM net_mon;";
 	my $dbMaxTime = $dbh->selectrow_array($sqlSMaxTime) || undef;
 
-	my $sqlSNodes = "SELECT * FROM net_mon WHERE \
-	log_time = '$dbMaxTime';";
+	my $sqlSNodes = sprintf("SELECT * FROM net_mon WHERE \
+	log_time = '%s' ORDER BY dn;", $dbMaxTime);
 	my $resultNodes = $dbh->selectall_arrayref($sqlSNodes, 
 		{ Slice => {} });
 	my @nodes = qw();
-	foreach my $node (@$resultNodes) {
+	foreach my $node (@$resultNodes) 
+	{
 		my $name = $node->{dn};
 		my $cutoff = index($name, ',');
-		if (($cutoff - 3) > 0) {
+		if (($cutoff - 3) > 0) 
+		{
 			$cutoff = $cutoff - 3;
 		}
 		$name = substr($name, 3, $cutoff);
 		my $state = 'chyba';
 		my $stateClass = 'state-fail';
-		if ($node->{state} == 1) {
+		if ($node->{state} == 1) 
+		{
 			$state = 'ok';
 			$stateClass = 'state-ok';
-		}
+		} # if node->{state}
 		my %item = ( NODE => $name,
 			STATECLASS => $stateClass,
 			STATE => $state,
 		);
 		push(@nodes, \%item);
-	}
-	my $tmplFile = $tmplPath.'/net-mon.tmpl';
+	} # foreach $node
+	my $tmplFile = sprintf("%s/net-mon.tmpl", $tmplPath);
 	my $template = HTML::Template->new(filename => $tmplFile);
 	$template->param('LASTCHECK', $dbMaxTime);
 	$template->param(NODES => \@nodes);
 	my $date = `/usr/bin/date +'%Y-%m-%d %H:%M:%S'`;
 	$template->param('DATECURR', $date);
 	return $template->output;
-}
+} # sub start
 
 1;
